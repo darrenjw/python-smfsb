bff1dcba8fa73b7819cd82b0f89c3b88ef1d6618
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2024-01-07 20:51:55 +0000

    more demos

---
+++ demos/s-m-lv-euler.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import smfsb
+import smfsb.models
+
+lv = smfsb.models.lv()
+print(lv)
+stepLv = lv.stepEuler(0.001)
+out = smfsb.simTs(lv.m, 0, 100, 0.1, stepLv)
+
+import matplotlib.pyplot as plt
+fig, axis = plt.subplots()
+for i in range(2):
+	axis.plot(range(out.shape[0]), out[:,i])
+
+axis.legend(lv.n)
+fig.savefig("s-m-lv-euler.pdf")
+
+# eof
