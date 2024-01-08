3d164d3142e05224573d86e1ff40aecc6b8af3f6
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2024-01-08 17:30:34 +0000

    more conf

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../src/smfsb/'))
 
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
