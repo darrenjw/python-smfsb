d233c06d6b73b8c70ba6e60a6010636fd38dbff5
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2024-01-16 22:27:26 +0000

    some demos

---
+++ demos/rcfmc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# rcfmc.py
+
+import smfsb
+import numpy as np
+import matplotlib.pyplot as plt
+
+Q = np.array([[-0.5,0.5],[1,-1]])
+pi0 = np.array([0.5,0.5])
+times, states = smfsb.rcfmc(30, Q, pi0)
+
+fig, axis = plt.subplots()
+axis.step(times, states[1:], where="post")
+
+fig.savefig("rcfmc.pdf")
+
+
+# eof
---
+++ demos/rfmc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# rfmc.py
+
+import smfsb
+import numpy as np
+import matplotlib.pyplot as plt
+
+P = np.array([[0.9,0.1],[0.2,0.8]])
+pi0 = np.array([0.5,0.5])
+out = smfsb.rfmc(200, P, pi0)
+
+fig, axis = plt.subplots()
+axis.step(range(200), out)
+
+fig.savefig("rfmc.pdf")
+
+
+# eof
