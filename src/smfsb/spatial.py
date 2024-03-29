fde15544812bebeb82f8bf1de0913eb904f9229c
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2024-03-29 14:18:47 +0000

    simTs1D

--- src/smfsb/spatial.py
+++ src/smfsb/spatial.py
@@ -1,6 +1,22 @@
 # spatial code from chapter 9
 
+import numpy as np
 
+def simTs1D(x0, t0, tt, dt, stepFun, verb=False):
+    N = int((tt - t0)//dt + 1)
+    u, n = x0.shape
+    arr = np.zeros((u, n, N))
+    x = x0
+    t = t0
+    arr[:,:,0] = x
+    for i in range(1, N):
+        if (verb):
+            print(N-i)
+        t = t + dt
+        x = stepFun(x, t, dt)
+        arr[:,:,i] = x
+    return(arr)
+    
 
 # eof
 
--- tests/test_spatial.py
+++ tests/test_spatial.py
@@ -14,6 +14,18 @@ def test_stepGillespie1D():
     stepLv1d = lv.stepGillespie1D(np.array([0.6, 0.6]))
     x1 = stepLv1d(x0, 0, 1)
     assert(x1.shape == (2,N))
+
+def test_simTs1D():
+    N=8
+    T=6
+    x0 = np.zeros((2,N))
+    lv = smfsb.models.lv()
+    x0[:,int(N/2)] = lv.m
+    stepLv1d = lv.stepGillespie1D(np.array([0.6, 0.6]))
+    out = smfsb.simTs1D(x0, 0, T, 1, stepLv1d)
+    assert(out.shape == (2,N,T+1))
+
+
     
 # eof
 
