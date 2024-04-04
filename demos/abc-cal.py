64d0f63ee7c6c5d019354b218f5230fb1410d9b8
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2024-04-04 18:35:59 +0100

    working on abc demos

--- demos/abc-cal.py
+++ demos/abc-cal.py
@@ -1,10 +1,11 @@
-# abc.py
+# abc-cal.py
+# ABC with calibrated summary stats
 
 import smfsb
 import numpy as np
 import scipy as sp
 
-print("ABC")
+print("ABC with calibrated summary stats")
 
 data = smfsb.data.LVperfect[:,range(1,3)]
 
@@ -17,8 +18,38 @@ def rmod(th):
   return smfsb.simTs([50, 100], 0, 30, 2,
                      smfsb.models.lv(th).stepCLE(0.1))
 
+def ss1d(vec):
+  n = len(vec)
+  mean = np.nanmean(vec)
+  v0 = vec - mean
+  var = np.nanvar(v0)
+  acs = [np.corrcoef(v0[range(n-1)], v0[range(1,n)])[0,1],
+         np.corrcoef(v0[range(n-2)], v0[range(2,n)])[0,1],
+         np.corrcoef(v0[range(n-3)], v0[range(3,n)])[0,1]]
+  #print(mean)
+  #print(var)
+  #print(acs)
+  return np.array([mean, np.log(var+1), acs[0], acs[1], acs[2]])
+
+def ssi(ts):
+  return np.concatenate((ss1d(ts[:,0]), ss1d(ts[:,1]),
+                         [np.corrcoef(ts[:,0], ts[:,1])[0,1]]))
+
+print("Pilot run")
+
+p, d = smfsb.abcRun(1000, rpr, lambda th: ssi(rmod(th)))
+prmat = np.vstack(p)
+dmat = np.vstack(d)
+print(prmat.shape)
+print(dmat.shape)
+sds = np.nanstd(dmat, 0)
+print(sds)
+
+
+print("Main run with calibrated summary stats")
+
 def sumStats(dat):
-  return dat
+  return ssi(dat)/sds
 
 ssd = sumStats(data)
 
@@ -29,9 +60,9 @@ def dist(ss):
 def rdis(th):
   return dist(sumStats(rmod(th)))
 
-p, d = smfsb.abcRun(1000000, rpr, rdis, verb=True)
+p, d = smfsb.abcRun(10000, rpr, rdis, verb=True)
 
-q = np.nanquantile(d, 0.01)
+q = np.nanquantile(d, 0.1)
 prmat = np.vstack(p)
 postmat = prmat[d < q,:]
 its, var = postmat.shape
@@ -47,7 +78,7 @@ axes[0, 2].scatter(postmat[:,1], postmat[:,2], s=0.5)
 axes[1, 0].hist(postmat[:,0], bins=30)
 axes[1, 1].hist(postmat[:,1], bins=30)
 axes[1, 2].hist(postmat[:,2], bins=30)
-fig.savefig("abc.pdf")
+fig.savefig("abc-cal.pdf")
 
 print("All done.")
 
