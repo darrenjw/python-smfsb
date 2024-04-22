3150413169cdb746daba00c917380587a2532d7f
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2024-04-22 15:21:54 +0100

    cle example (very slow)

---
+++ demos/time-lv-cle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# time-lv-cle.py
+# time the CLE algorithm
+
+import numpy as np
+import scipy as sp
+import smfsb
+import matplotlib.pyplot as plt
+import time
+
+lvmod = smfsb.models.lv()
+step = lvmod.stepCLE(0.01)
+
+## Start timer
+startTime = time.time()
+out = smfsb.simSample(10000, lvmod.m, 0, 20, step)
+endTime = time.time()
+## End timer
+elapsedTime = endTime - startTime
+print(f"\n\nElapsed time: {elapsedTime} seconds\n\n")
+
+out = np.where(out > 1000, 1000, out)
+print(sp.stats.describe(out))
+fig, axes = plt.subplots(2,1)
+for i in range(2):
+    axes[i].hist(out[:,i], bins=50)
+fig.savefig("time-lv-cle.pdf")
+
+
+# eof
+
