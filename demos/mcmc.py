6447101bdcfebb33ea4902214d3499e368b71794
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2026-09-06 11:38:11 +0100

    better mcmc summary plots

--- demos/mcmc.py
+++ demos/mcmc.py
@@ -17,7 +17,13 @@ def acf(x, lag_max):
     return np.array([1] + [np.corrcoef(x[:-i], x[i:])[0, 1] for i in range(1, lag_max)])
 
 
-def mcmc_summary(mat, file_name="mcmc.pdf", bins=30, lag_max=100, show=True, plot=True):
+def mcmc_summary(
+        mat, 
+        file_name="mcmc.pdf", 
+        labels=False, 
+		truth=False,
+        bins=30, lag_max=100, show=True, plot=True
+		):
     n, p = mat.shape
     summ = sp.stats.describe(mat)
     med = np.median(mat, 0)
@@ -30,13 +36,23 @@ def mcmc_summary(mat, file_name="mcmc.pdf", bins=30, lag_max=100, show=True, plo
         print(f"Max: {summ.minmax[1]}")
     if plot:
         fig, axes = plt.subplots(p, 3)
+        fig.tight_layout()
         for i in range(p):
-            axes[i, 0].plot(range(n), mat[:, i], linewidth=0.1)
+            axes[i, 0].plot(range(n), mat[:, i], linewidth=0.4)
+            axes[i, 0].set_title('Traceplot')
+            if labels:
+                axes[i, 0].set_ylabel(labels[i])
+            if truth:
+                axes[i, 0].hlines(truth[i], 0, n, 'r')
             axes[i, 1].plot(range(lag_max), acf(mat[:, i], lag_max))
             axes[i, 1].set_ylim([-0.5, 1])
-            axes[i, 1].axhline(y=0, color="g", linewidth=0.5)
-            axes[i, 2].hist(mat[:, i], bins=bins)
-        fig.savefig(file_name)
+            axes[i, 1].set_title('ACF')
+            axes[i, 1].axhline(y=0, color="g", linewidth=0.6)
+            h, b, bc = axes[i, 2].hist(mat[:, i], bins=bins, density=True)
+            axes[i, 2].set_title('Density')
+            if truth:
+                axes[i, 2].vlines(truth[i], 0, np.max(h), 'r')
+        fig.savefig(file_name, dpi=300, bbox_inches='tight')
     return summ
 
 
